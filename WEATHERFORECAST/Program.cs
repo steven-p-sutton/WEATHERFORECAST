efb65754e2a577741d06c770745761542dac1640
Steve Sutton <steven.p.sutton@gmail.com>
2020-10-07 08:45:43 +0100

    Initial creation

---
+++ WEATHERFORECAST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEATHERFORECAST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
